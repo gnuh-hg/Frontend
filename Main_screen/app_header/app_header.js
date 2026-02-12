50d7bba686c795b5b7c8b2351d969d66feceb828
Hung <nmhung29042009@gmail.com>
2026-02-12 21:54:28 +0700

    kbt

---
+++ Main_screen/app_header/app_header.js
@@ -0,0 +1,10 @@
+import * as Config from '../../constants.js';
+
+document.addEventListener('DOMContentLoaded', function() {
+    const username = document.querySelector('.username');
+    username.addEventListener('click', function() {
+        localStorage.removeItem('access_token'); // Xóa token
+        
+        window.location.href = '../Account/login.html';
+    });
+});
